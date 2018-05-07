0a48e9a1e600be0964af0ae3181b7db763612f48
Dewey Sasser <dewey@sasser.com>
2018-05-07 07:59:34 -0400

    Add changer interface

--- connection/connection.go
+++ connection/connection.go
@@ -8,3 +8,8 @@ type Connection interface {
 	Fetch() (keys <-chan data.Key, accounts <-chan data.Account)
 	Id() data.ID
 }
+
+type Changer interface {
+	Add(bindings []data.KeyBinding)
+	Remove(bindings []data.KeyBinding)
+}
