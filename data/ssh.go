a72043f78f635849eac695aa8ac36f71e2af6eb0
Dewey Sasser <dewey@deweysasser.com>
2021-05-31 20:52:56 -0400

    Update to current go standards.
    Unfortunately, the AWS Public key interfaces we were depending on has been removed, so that
    functionality has been commented out for now.

--- data/ssh.go
+++ data/ssh.go
@@ -161,15 +161,17 @@ func (key *SSHKey) Identifiers() []ID {
 			key.Ids.Add(ID(ssh.FingerprintSHA256(key.PublicKey.Key)))
 			key.Ids.Add(ID(ssh.FingerprintLegacyMD5(key.PublicKey.Key)))
 			// Complexity here:  we have an ssh.rsaPublicKey but need to turn it into an rsa.PublicKey
-			if cpker, ok := key.PublicKey.Key.(ssh.CryptoPublicKey); ok {
-				if rsapk, ok := cpker.CryptoPublicKey().(*rsa.PublicKey); ok {
-						if fp, err := ssh.FingerprintAWSPublic(rsapk); err == nil {
-							key.Ids.Add(ID(fp))
-						} else {
-							output.Warn("Failed to compute AWS format fingerprint:", err)
-						}
-					}
-				}
+
+			// TODO:  This has been changed since the code was originally written.  Find the new way of doing this.
+			//if cpker, ok := key.PublicKey.Key.(ssh.CryptoPublicKey); ok {
+			//	if rsapk, ok := cpker.CryptoPublicKey().(*rsa.PublicKey); ok {
+			//			if fp, err := ssh.FingerprintAWSPublic(rsapk); err == nil {
+			//				key.Ids.Add(ID(fp))
+			//			} else {
+			//				output.Warn("Failed to compute AWS format fingerprint:", err)
+			//			}
+			//		}
+			//	}
 			}
 		key.haveIdsBeenAdded = true
 	}
